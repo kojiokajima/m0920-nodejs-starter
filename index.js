9736e9d2fa9b1fd9d8053245166d5cda6db25a2e
Francois Polo <andasan@gmail.com>
2021-05-10 00:45:11 -0700

    extra changes to starter file, removed bodyparser module

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 const adminRouters = require('./routes/admin');
 const shopRouters = require('./routes/shop');
-const rootDirectory = require('./util/path');
 
 //--------------------Setups--------------------
 const app = express();
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.urlencoded({extended:false}));
 
 //app.set = allows us to set any values globally on our express application
 app.set('view engine', 'ejs');
@@ -24,8 +22,6 @@ app.use(shopRouters);
 
 // catch all middleware
 app.use((req,res,next)=>{
-    // res.status(404).send('<h1>Page not found</h1>');
-    // res.sendFile(path.join(rootDirectory, 'views', '404.html'));
     res.render('404', {
         pageTitle: 'Page not found'
     });
